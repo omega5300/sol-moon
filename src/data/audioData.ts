676c472ef296fe9bb8ae46203a880e6eb3443586
omega5300 <omegamatrix5351@gmail.com>
2023-03-10 12:42:28 -0500

    final version

--- src/data/audioData.ts
+++ src/data/audioData.ts
@@ -4,32 +4,33 @@ export interface Track {
 }
 
 export const trackList: Track[] = [
-  { title: 'austan', url: 'music/01-austan.m4a', },
-  { title: 'journey', url: 'music/02-journey.m4a', },
-  { title: 'yore', url: 'music/03-yore.m4a', },
-  { title: 'seyyahi2', url: 'music/04-seyyahi2.m4a', },
-  { title: 'paradise', url: 'music/05-paradise.m4a', },
-  { title: 'suna empire', url: 'music/06-suna_empire.m4a', },
-  { title: 'midian', url: 'music/07-midian.m4a', },
-  { title: 'rhythm empire', url: 'music/08-rhythm_empire.m4a', },
-  { title: 'inner-control', url: 'music/09-inner-control.m4a', },
-  { title: 'voyage', url: 'music/10-voyage.m4a', },
+
   {
-    title: 'rise of the warrior',
-    url: 'music/11-rise_of_the_warrior.m4a',
+    url: 'https://www.bensound.com/bensound-music/bensound-happyrock.mp3',
+    title: 'Happy Rock',
   },
-  { title: 'vestan', url: 'music/12-vestan.m4a', },
-  { title: 'melancholic', url: 'music/13-melancholic.m4a', },
-  { 
-    title: 'multilacion mortal 2', 
-    url: 'music/14-multilacion_mortal_2.m4a', 
+  {
+    url: 'https://www.bensound.com/bensound-music/bensound-anewbeginning.mp3',
+    title: 'Punky',
   },
-  { title: 'war', url: 'music/15-war.m4a', },
-  { 
-    title: 'brutal assertivity 2', 
-    url: 'music/16-brutal_assertivity_2.m4a', 
+  {
+    url: 'https://www.bensound.com/bensound-music/bensound-actionable.mp3',
+    title: 'Actionable',
   },
-  { title: 'hellfire', url: 'music/17-hellfire.m4a', },
-  { title: 'void', url: 'music/18-void.m4a', },
-  { title: 'huzur', url: 'music/19-huzur.m4a', },
+  {
+    url: 'https://www.bensound.com/bensound-music/bensound-romantic.mp3',
+    title: 'Romantic',
+  },
+  {
+    url: 'https://www.bensound.com/bensound-music/bensound-allthat.mp3',
+    title: 'All That',
+  },
+  {
+    url: 'https://www.bensound.com/bensound-music/bensound-love.mp3',
+    title: 'Love',
+  },
+  {
+    url: 'https://www.bensound.com/bensound-music/bensound-dreams.mp3',
+    title: 'Dreams',
+  },
 ]
